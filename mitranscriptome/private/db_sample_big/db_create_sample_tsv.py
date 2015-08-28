7642523e2d543e5031b4cc32c9806a2c763a75e1
yniknafs <yniknafs@umich.edu>
2015-08-28 16:50:21 -0400

    added large sample data
    6,500 samples
    100 genes

---
+++ mitranscriptome/private/db_sample_big/db_create_sample_tsv.py
@@ -0,0 +1,51 @@
+'''
+Turns a tab-delimited text file into JSON for import using mongoimport
+
+Assumptions:
+1) First line is a 'header' line
+2) The first column is the 'key' column
+3) Subsequent columns contain numeric data
+'''
+import sys
+import os
+import json
+import argparse
+import logging
+
+'''
+python db_create_sample_tsv.py /Volumes/dx11/mctp/projects/ssea/isoform_count_matrix_v7/colnames.txt /Volumes/dx11/mctp/projects/ssea/isoform_count_matrix_v7/colmeta.tsv > samples.big.tsv
+'''
+
+def main():
+    # parse command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ordered_colnames")
+    parser.add_argument("sample_metadata")
+
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.DEBUG,
+                      format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+    tissue_dict = {}
+    fileh = open(args.sample_metadata)
+    header = fileh.next().strip().split('\t')
+    for line in fileh:
+        line = line.strip().split('\t')
+        library_id = line[header.index("library_id")].lower()
+        tissue = line[header.index("cohort")]
+        tissue_dict[library_id] = tissue
+
+    i=0
+    headero = ['library_id', 'tissue']
+    print '\t'.join(headero)
+    for library_id in open(args.ordered_colnames):
+        library_id = library_id.strip().lower()
+        tissue = tissue_dict[library_id]
+        lineo = [library_id, tissue]
+        print '\t'.join(lineo)
+        i+=1
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ mitranscriptome/private/db_sample_big/db_create_transcript_tsv.py
@@ -0,0 +1,53 @@
+'''
+Turns a tab-delimited text file into JSON for import using mongoimport
+
+Assumptions:
+1) First line is a 'header' line
+2) The first column is the 'key' column
+3) Subsequent columns contain numeric data
+'''
+import sys
+import os
+import json
+import argparse
+import logging
+
+'''
+python db_create_transcript_tsv.py /Volumes/dx11/mctp/projects/ssea/isoform_count_matrix_v7/rownames.txt /Volumes/dx11/mctp/projects/mitranscriptome/annotation/metadata.transcript.txt > transcripts.big.tsv
+'''
+
+def main():
+    # parse command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ordered_rownames")
+    parser.add_argument("transcript_metadata")
+    parser.add_argument("-n", dest = "number", default = 100)
+
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.DEBUG,
+                      format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+    tmeta_dict = {}
+    fileh = open(args.transcript_metadata)
+    metaheader = fileh.next().strip().split('\t')
+    for line in fileh:
+        line = line.strip().split('\t')
+        transcript_id = line[0]
+        meta = line[0:10]
+        tmeta_dict[transcript_id] = meta
+
+    i=0
+    headero = metaheader[0:10]
+    print '\t'.join(headero)
+    for transcript_id in open(args.ordered_rownames):
+        if i >= args.number: continue
+        transcript_id = transcript_id.strip()
+        meta = tmeta_dict[transcript_id]
+        lineo = meta
+        print '\t'.join(lineo)
+        i+=1
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
